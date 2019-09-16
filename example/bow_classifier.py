e57252ba62dcb7f72b6261bc0b7973188bad7f9e
Kohei Moroi <koheimoroi@Koheis-MacBook-Air.local>
2019-09-16 22:26:55 +0900

    add Word2VecVectorizer to classify enourmous words with GoogleNews-vectors

--- example/bow_classifier.py
+++ example/bow_classifier.py
@@ -78,6 +78,56 @@ class GloveVectorizer:
         return self.transform(data)
 
 
+class Word2VecVectorizer:
+
+    def __init__(self):
+        print("Loading in word vectors...")
+        # you may need to download the pretrained data set using
+        # wget -c "http://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz"
+        self.word_vectors = KeyedVectors.load_word2vec_format(
+            '../large_files/GoogleNews-vectors-negative300.bin',
+            binary=True
+        )
+        print("Finished loading in word vectors")
+
+    def fit(self, data):
+        pass
+
+    def transform(self, data):
+        # determine the dimensionality of vectors
+        v = self.word_vectors.get_vector('king')
+        self.D = v.shape[0]
+
+        X = np.zeros((len(data), self.D))
+        n = 0
+        emptycount = 0
+        for sentence in data:
+            tokens = sentence.split()
+            vecs = []
+            m = 0
+            for word in tokens:
+                try:
+                    # throws KeyError if word not found
+                    vec = self.word_vectors.get_vector(word)
+                    vecs.append(vec)
+                    m += 1
+                except KeyError:
+                    pass
+            if len(vecs) > 0:
+                vecs = np.array(vecs)
+                X[n] = vecs.mean(axis=0)
+            else:
+                emptycount += 1
+            n += 1
+        print("Numer of samples with no words found: %s / %s" %
+              (emptycount, len(data)))
+        return X
+
+    def fit_transform(self, data):
+        self.fit(data)
+        return self.transform(data)
+
+
 if __name__ == '__main__':
 
     vectorizer = GloveVectorizer()
